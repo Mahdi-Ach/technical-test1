78c17bf97b6f047348dde364115ae917d27831b7
XDIAG2001 <achbabmahdi120@gmail.com>
2023-12-30 18:55:14 +0100

    backend

---
+++ RestApi/list_users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get('/users', async (req, res) => {
+  try {
+    console.log('https://dummyjson.com/users/search?q='+req.query.criteria+"&limit="+req.query.limit+"&skip="+req.query.skip);
+    const response = await axios.get('https://dummyjson.com/users/search?q='+req.query.criteria+"&limit="+req.query.limit+"&skip="+req.query.skip);
+    const users = response.data;
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
